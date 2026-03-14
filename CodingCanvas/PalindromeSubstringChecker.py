36ce78a464f5d16fbeab776ce951b41fbc617c88
Anjali Priya <anji707951@gmail.com>
2026-03-14 13:54:37 +0530

    Update PalindromeSubstringChecker.py

--- CodingCanvas/PalindromeSubstringChecker.py
+++ CodingCanvas/PalindromeSubstringChecker.py
@@ -11,3 +11,24 @@ Constraints - 1 <= s.length <= 100000
 
 Hint
 Consider using a nested loop to generate all possible substrings and check if any of them is a palindrome. Remember, you only need to check substrings of length greater than 1.
+
+
+
+#SOLUTION -
+
+def has_palindrome_substring(test):
+    n = len(test)
+    
+    for i in range(n):
+        #Check Length 2 Plaindrome
+        if i+1 < n and test[i]==test[i+1]:
+            return "TRUE" 
+        #Check Length 3 Plaindrome
+        if i+2 < n and test[i]==test[i+2]:
+            return "TRUE"  
+    return "NO"
+
+
+test = "abccddccba"
+print(test == test[::-1]) # Easiest way to check not ideal
+print(has_palindrome_substring(test))
