bc96aa418d99f256f3f53eae65ce88e89c9103b1
Anjali Priya <anjalipriya@Anjalis-MacBook-Air.local>
2026-08-14 10:15:10 +0530

    code commit

---
+++ Python/file-and-dataprocessing/practice.py
@@ -0,0 +1,23 @@
+#Given millions of log lines, find the top 10 error types without loading the entire file into memory.
+
+uniqe_errors = {}
+# count_connection_refused = 0
+with open('./Misc/sample_app.log') as f:
+    for line in f:
+
+        if "ERROR" in line:
+            split_list= line.split(" ")
+
+            error_type = split_list[-1].strip("message=\n")
+
+            if error_type not in uniqe_errors:
+                uniqe_errors[error_type] = 0
+            
+            uniqe_errors[error_type] = uniqe_errors[error_type] + 1
+
+
+print(uniqe_errors)
+# {
+#     PermissionDenied : '',
+#     FileNotFound : ''
+# }
--- Python/fileshandlingbasics/fileread.py
+++ Python/fileshandlingbasics/fileread.py
@@ -1,23 +1,35 @@
-#Given millions of log lines, find the top 10 error types without loading the entire file into memory.
+# 125. Create a file and write some content.
+def create_file():
+    with open("./Misc/outputfile.txt", "w") as file:
+        file.write( 
+            "Hello, this is my first file.\n"
+            "I am learning Python file handling.\n"
+            "This content is written using Python."
+            )
 
-uniqe_errors = {}
-# count_connection_refused = 0
-with open('../sample_app.log') as f:
-    for line in f:
+create_file()
 
-        if "ERROR" in line:
-            split_list= line.split(" ")
+#126. Read the contents of a file. Entire file → read()
+def readcontentfromfile():
+    with open("./Misc/sample_app.log", "r") as file:
+        content=file.read()
+        print(content)
 
-            error_type = split_list[-1].strip("message=\n")
+readcontentfromfile()
 
-            if error_type not in uniqe_errors:
-                uniqe_errors[error_type] = 0
-            
-            uniqe_errors[error_type] = uniqe_errors[error_type] + 1
+#127. Print every line of a file. - The requirement is to process/print one line at a time. Line by line → for line in file
+def readcontentfromfile():
+    with open("./Misc/outputfile.txt", "r") as file:
+        for line in file:
+            print(line.strip()) #strip() → removes leading/trailing whitespace, including \n.
 
+readcontentfromfile()
 
-print(uniqe_errors)
-# {
-#     PermissionDenied : '',
-#     FileNotFound : ''
-# }
+#128. Count the number of lines.
+def countnooflinesinfile():
+    count = 0
+    with open("./Misc/outputfile.txt","r") as file:
+        for line in file:
+            count+=1
+        return count
+print(countnooflinesinfile())
