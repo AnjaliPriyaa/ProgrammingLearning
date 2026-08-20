7c05cc4095b3cc87630ab8f02a406df6a6308975
Anjali Priya <anji707951@gmail.com>
2026-08-20 17:27:22 +0530

    [283] [Time Beats: 15.91%] [Memory Beats: 24.74%] - LeetPush
    Refactor zero-moving logic for optimization.

--- LeetCode-Solutions/283-Move-Zeroes.py
+++ LeetCode-Solutions/283-Move-Zeroes.py
@@ -4,9 +4,15 @@ class Solution:
         Do not return anything, modify nums in-place instead.
 
         """
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                for j in range(i,len(nums)):
+                    if nums[j]!=0:
+                        nums[i],nums[j]=nums[j],nums[i]
+                        break
+        #Optimized
         move = 0
         for i in range(len(nums)):
             if nums[i]!=0:
                 nums[i],nums[move]=nums[move],nums[i]
                 move+=1
-        
