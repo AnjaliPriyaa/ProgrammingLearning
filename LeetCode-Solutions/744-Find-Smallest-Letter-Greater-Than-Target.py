19c2489784207488d5ded9dacb49d2ada09706cd
Anjali Priya <anji707951@gmail.com>
2026-08-21 18:43:22 +0530

    [744] [Time Beats: 100.00%] [Memory Beats: 86.48%] - LeetPush

---
+++ LeetCode-Solutions/744-Find-Smallest-Letter-Greater-Than-Target.py
@@ -0,0 +1,21 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        # Brute force / Linear scan → O(n) time, O(1) space
+        # for i in letters:
+        #     if i > target:
+        #         return i
+        # return letters[0]
+
+        # Optimal / Binary search   → O(log n) time, O(1) space
+        left = 0
+        right = len(letters) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if letters[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return letters[left % len(letters)]
