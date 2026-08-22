87642f8f34023ad1b7e66fb6780958f60a5aa246
Anjali Priya <anji707951@gmail.com>
2026-08-22 17:07:04 +0530

    [20] [Time Beats: 31.93%] [Memory Beats: 91.07%] - LeetPush

---
+++ LeetCode-Solutions/20-Valid-Parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pairs = {
+            ')': '(',
+            ']': '[',
+            '}': '{'
+        }
+        for bracket in s:
+            if bracket in "{[(":
+                stack.append(bracket)
+            else:
+                if not stack:
+                    return False
+                if stack[-1] != pairs[bracket]:
+                    return False
+                stack.pop()
+        return len(stack) == 0
+
+                
+
+
+
+        
