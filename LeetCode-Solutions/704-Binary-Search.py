c69625575afeaaa45f1bd3b69437faa08c547618
Anjali Priya <anji707951@gmail.com>
2026-08-24 19:15:15 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 36.65%] - LeetPush

--- LeetCode-Solutions/704-Binary-Search.py
+++ LeetCode-Solutions/704-Binary-Search.py
@@ -6,7 +6,7 @@ class Solution:
             mid = (left+right) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] < target:
+            elif nums[mid] < target:
                 left = mid + 1
             else:
                 right= mid - 1
